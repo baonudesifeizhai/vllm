a0bee72906d965ea0869c80c3dd4610eef90cafd
baonudesifeizhai <baonudesifeizhai@gmail.com>
2026-02-16 03:07:01 -0500

    fix
    Signed-off-by: baonudesifeizhai <baonudesifeizhai@gmail.com>

--- csrc/cutlass_extensions/epilogue/broadcast_load_epilogue_array_c3x.hpp
+++ csrc/cutlass_extensions/epilogue/broadcast_load_epilogue_array_c3x.hpp
@@ -189,15 +189,19 @@ struct Sm90RowOrScalarBroadcastArray {
       }
 
       auto synchronize = [&] () { cutlass::arch::NamedBarrier::sync(thr_num, cutlass::arch::ReservedNamedBarriers::EpilogueBarrier); };
-      for (int i = 0; i < size(tGS_gRow); ++i) {
-        if (get<1>(tGS_cRow(i)) >= size<1>(CtaTileShapeMNK{})) {
+      Tensor tGS_gRow_flt = filter_zeros(tGS_gRow);
+      Tensor tGS_sRow_flt = filter_zeros(tGS_sRow);
+      Tensor tGS_cRow_flt = make_tensor(tGS_cRow.data(), make_layout(tGS_gRow_flt.shape(), tGS_cRow.stride()));
+
+      for (int i = 0; i < size(tGS_gRow_flt); ++i) {
+        if (get<1>(tGS_cRow_flt(i)) >= size<1>(CtaTileShapeMNK{})) {
           continue; // OOB of SMEM, 
         }
-        if (elem_less(tGS_cRow(i), make_coord(get<0>(residue_tCcRow), get<1>(residue_tCcRow)))) {
-          tGS_sRow(i) = tGS_gRow(i);
+        if (elem_less(tGS_cRow_flt(i), make_coord(get<0>(residue_tCcRow), get<1>(residue_tCcRow)))) {
+          tGS_sRow_flt(i) = tGS_gRow_flt(i);
         }
         else {
-          tGS_sRow(i) = Element(0); // Set to Zero when OOB so LDS could be issue without any preds.
+          tGS_sRow_flt(i) = Element(0); // Set to Zero when OOB so LDS could be issue without any preds.
         }
       }
       synchronize();
@@ -207,9 +211,8 @@ struct Sm90RowOrScalarBroadcastArray {
     begin_loop(int epi_m, int epi_n) {
       if (epi_m == 0) { // Assumes M-major subtile loop
         if (!params.row_broadcast) return; // Do not issue LDS when row is scalar
-        Tensor tSR_sRow_flt = filter_zeros(tSR_sRow(_,_,_,epi_m,epi_n));
-        Tensor tSR_rRow_flt = filter_zeros(tSR_rRow);
-        copy(tSR_sRow_flt, tSR_rRow_flt);
+        Tensor tSR_sRow_cur = tSR_sRow(_, _, _, epi_m, epi_n);
+        copy(tSR_sRow_cur, tSR_rRow);
       }
     }
 
